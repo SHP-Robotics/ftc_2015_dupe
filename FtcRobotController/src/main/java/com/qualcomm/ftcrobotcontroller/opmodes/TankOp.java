1b77ea76369bee4f396764555c63ae18749d3d5e
Andrew Zhu <mommothazaz123@gmail.com>
2015-09-24 13:15:50 -0700

    Updated TankOp for Friction wheel

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankOp.java
@@ -1,48 +1,132 @@
+/* Copyright (c) 2014 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by andrew on 9/18/15.
+ * TeleOp Mode
+ * <p>
+ * Enables control of the robot via the gamepad
  */
 public class TankOp extends OpMode {
 
-    DcMotor left, right, conveyor, grapple, grappleGuide;
 
-    public void init() {
-        left = hardwareMap.dcMotor.get("motor_1");
-        right = hardwareMap.dcMotor.get("motor_2");
-        conveyor = hardwareMap.dcMotor.get("conveyor");
-        grapple = hardwareMap.dcMotor.get("winch");
-        grappleGuide = hardwareMap.dcMotor.get("grapple");
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+    DcMotor motorBoost;
+
+
+    /**
+     * Constructor
+     */
+    public TankOp() {
+
     }
 
+    /*
+     * Code to run when the op mode is initialized goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#init()
+     */
+    @Override
+    public void init() {
+
+
+		/*
+		 * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+        motorRight = hardwareMap.dcMotor.get("motor_2");
+        motorLeft = hardwareMap.dcMotor.get("motor_1");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBoost = hardwareMap.dcMotor.get("motor_3");
+
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+     */
+    @Override
     public void loop() {
+
+
         float throttle = -gamepad1.left_stick_y;
         float direction = gamepad1.left_stick_x;
-        float rightValue = throttle - direction;
-        float leftValue = throttle + direction;
+        float right = throttle - direction;
+        float left = throttle + direction;
 
         // clip the right/left values so that the values never exceed +/- 1
-        rightValue = Range.clip(rightValue, -1, 1);
-        leftValue = Range.clip(leftValue, -1, 1);
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
 
         // scale the joystick value to make it easier to control
         // the robot more precisely at slower speeds.
-        rightValue = (float)scaleInput(rightValue);
-        leftValue =  (float)scaleInput(leftValue);
+        right = (float)scaleInput(right);
+        left =  (float)scaleInput(left);
 
         // write the values to the motors
-        right.setPower(rightValue);
-        left.setPower(leftValue);
+        motorRight.setPower(right);
+        motorLeft.setPower(left);
+        motorBoost.setPower(-throttle);
+
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
+        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
+
     }
 
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+     */
+    @Override
     public void stop() {
 
     }
 
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
     double scaleInput(double dVal)  {
         double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
@@ -64,4 +148,5 @@ public class TankOp extends OpMode {
 
         return dScale;
     }
+
 }
